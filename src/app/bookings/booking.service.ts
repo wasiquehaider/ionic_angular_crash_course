03a1c818874d85afdddcc895e468718d1aea82e3
Wasique Haider <whaider@digital-dividend.com>
2021-06-03 17:20:02 +0500

    fetch bookings

--- src/app/bookings/booking.service.ts
+++ src/app/bookings/booking.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { take, tap, delay, switchMap } from 'rxjs/operators';
+import { take, tap, delay, switchMap, map } from 'rxjs/operators';
 
 import { Booking } from './booking.model';
 import { AuthService } from '../auth/auth.service';
 import { HttpClient } from '@angular/common/http';
+
+interface BookingData {
+  bookedFrom: string;
+  bookedTo: string;
+  firstName: string;
+  guestNumber: number;
+  lastName: string;
+  placeId: string;
+  placeImage: string;
+  placeTitle: string;
+  userId: string;
+}
 
 @Injectable({ providedIn: 'root' })
 export class BookingService {
@@ -65,5 +77,38 @@ export class BookingService {
         this._bookings.next(bookings.filter((b) => b.id !== bookingId));
       })
     );
+  }
+  fetchBookings() {
+    return this.http
+      .get<{ [key: string]: BookingData }>(
+        `https://ionic-database-635d2-default-rtdb.firebaseio.com/bookings.json?orderBy="userId"&equalTo="${this.authService.userId}"`
+      )
+      .pipe(
+        map((bookingData) => {
+          const bookings = [];
+          for (const key in bookingData) {
+            if (bookingData.hasOwnProperty(key)) {
+              bookings.push(
+                new Booking(
+                  key,
+                  bookingData[key].placeId,
+                  bookingData[key].userId,
+                  bookingData[key].placeTitle,
+                  bookingData[key].placeImage,
+                  bookingData[key].firstName,
+                  bookingData[key].lastName,
+                  bookingData[key].guestNumber,
+                  new Date(bookingData[key].bookedFrom),
+                  new Date(bookingData[key].bookedTo)
+                )
+              );
+            }
+          }
+          return bookings;
+        }),
+        tap((bookings) => {
+          this._bookings.next(bookings);
+        })
+      );
   }
 }
