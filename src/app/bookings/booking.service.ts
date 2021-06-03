da4af884efd4f837caa02c746d9ceaca62d60d3f
Wasique Haider <whaider@digital-dividend.com>
2021-06-03 17:27:09 +0500

    delete booking

--- src/app/bookings/booking.service.ts
+++ src/app/bookings/booking.service.ts
@@ -70,13 +70,19 @@ export class BookingService {
   }
 
   cancelBooking(bookingId: string) {
-    return this.bookings.pipe(
-      take(1),
-      delay(1000),
-      tap((bookings) => {
-        this._bookings.next(bookings.filter((b) => b.id !== bookingId));
-      })
-    );
+    return this.http
+      .delete(
+        `https://ionic-database-635d2-default-rtdb.firebaseio.com/bookings/${bookingId}.json`
+      )
+      .pipe(
+        switchMap(() => {
+          return this.bookings;
+        }),
+        take(1),
+        tap((bookings) => {
+          this._bookings.next(bookings.filter((b) => b.id !== bookingId));
+        })
+      );
   }
   fetchBookings() {
     return this.http
